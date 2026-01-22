b3abb164092db5689abf5559977f77c5fc22e347
FeliciaBo <theskylla@gmail.com>
2026-01-22 20:01:14 +0100

    added markdown to movie intro

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { marked } from 'marked'
 
 const app = express()
 const port = 5080
@@ -43,10 +44,23 @@ app.get('/movies/:id', async (req, res) => {
     const response = await fetch(`https://plankton-app-xhkom.ondigitalocean.app/api/movies/${id}`);
     const result = await response.json();
 
-    res.render('single-movie', { movie: result.data });
+    const movie = result.data;
+
+ movie.attributes.introHtml = marked.parse(`
+# Markdown testing
+
+**Bold text**
+
+- One
+- Two
+- Three
+`);
+    
+    res.render('single-movie', { movie });
   } catch (error) {
     console.error('Error: Failed to fetch movie:', error);
     res.status(500).send('Internal Server Error');
+    console.log(error);
   }
 });
 
