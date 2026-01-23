6a0207fdfdbd9f856b8f89314aa52f6d6b33d75a
FeliciaBo <theskylla@gmail.com>
2026-01-23 21:06:46 +0100

    added function for items published since last visit + changed publishedAt on card to updatedAt

--- public/main.js
+++ public/main.js
@@ -2,6 +2,19 @@
 const lastVisit = localStorage.getItem('lastVisit');
 console.log('Last visited on:', lastVisit);
 
-//store current visit time
-const currentVisit = new Date().toISOString();
-localStorage.setItem('lastVisit', currentVisit);
+
+
+if (lastVisit) {
+    const lastVisitDate = new Date(lastVisit);
+
+    document.querySelectorAll('.movie-poster').forEach(card => {
+        const publishedAt = new Date(card.dataset.publishedAt);
+        
+
+        if (publishedAt > lastVisitDate) {
+            card.classList.add('new');
+        }
+    });
+}
+
+localStorage.setItem('lastVisit', new Date().toISOString());
